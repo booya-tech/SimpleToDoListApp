26fb1b3b258460445736bc4b49cf0c4cb2a34494
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-04 16:51:01 +0700

    first commit

---
+++ SimpleToDoListApp/SimpleToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoListAppApp.swift
+//  SimpleToDoListApp
+//
+//  Created by Panachai Sulsaksakul on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
