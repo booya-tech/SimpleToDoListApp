0b43ece3f1051834445255303a45e7f4a3cfdb9f
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-05 15:15:43 +0700

    feat: add task presenter

--- SimpleToDoListApp/Scenes/TaskList/TaskListPresenter.swift
+++ SimpleToDoListApp/Scenes/TaskList/TaskListPresenter.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+//MARK: - Protocols
+protocol TaskListDisplayLogic: AnyObject {
+    func displayTasks(viewModel: TaskList.FetchTask.ViewModel)
+    func displayToggleResult(viewModel: TaskList.ToggleTask.ViewModel)
+    func displayAddResult(viewModel: TaskList.AddTask.ViewModel)
+    func displayDeleteResult(viewModel: TaskList.DeleteTask.ViewModel)
+}
+
+//MARK: - Presenter Implementation
+class TaskListPresenter: TaskListPresentationLogic {
+    // Output: Talks back to View (WEAK to avoid retain cycle)
+    weak var viewController: TaskListDisplayLogic?
+    
+    func presentTasks(response: TaskList.FetchTask.Response) {
+        //TODO: - Format tasks for display
+    }
+    
+    func presentToggleResult(response: TaskList.ToggleTask.Response) {
+        //TODO: - Format toggle result
+    }
+    
+    func presentAddResult(response: TaskList.AddTask.Response) {
+        //TODO: - Format add result
+    }
+    
+    func presentDeleteResult(response: TaskList.DeleteTask.Response) {
+        //TODO: - Format delete result
+    }
+}
