37f3a41479de4b0b7610ba6eae0d946130487ab0
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-05 15:05:56 +0700

    feat, chore: (feat) add task interactor, (chore) update .gitignore

--- SimpleToDoListApp/Scenes/TaskList/TaskListInteractor.swift
+++ SimpleToDoListApp/Scenes/TaskList/TaskListInteractor.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+//MARK: - Protocols
+
+// What the View can ask the Interactor to do
+protocol TaskListBusinessLogic {
+    func fetchTasks()
+    func toggleTask(request: TaskList.ToggleTask.Request)
+    func addTask(request: TaskList.AddTask.Request)
+    func deleteTask(request: TaskList.DeleteTask.Request)
+}
+
+// What the Interactor uses to send results to Presenter
+protocol TaskListPresentationLogic {
+    func presentTasks(response: TaskList.FetchTask.Response)
+    func presentToggleResult(response: TaskList.ToggleTask.Response)
+    func presentAddResult(response: TaskList.AddTask.Response)
+    func presentDeleteResult(response: TaskList.DeleteTask.Response)
+}
+
+//MARK: - Interactor Implementation
+class TaskListInteractor: TaskListBusinessLogic {
+    // Output: Talks to Presenter
+    var presenter: TaskListPresentationLogic?
+    
+    // Dependency: Worker that handles storage
+    //TODO: - Implement
+    //    var worker: TaskStoreWorkerProtocol?
+    
+    
+    // Business Logic Methods
+    func fetchTasks() {
+        //TODO: - Implement
+    }
+    
+    func toggleTask(request: TaskList.ToggleTask.Request) {
+        //TODO: - Implement
+    }
+    
+    func addTask(request: TaskList.AddTask.Request) {
+        //TODO: - Implement
+    }
+    
+    func deleteTask(request: TaskList.DeleteTask.Request) {
+        //TODO: - Implement
+    }
+}
